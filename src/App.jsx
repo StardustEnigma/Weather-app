8d5d2488fbf03781ea5fdccfc96f8885493b1136
Atharva <atharvamandle19@gmail.com>
2025-02-09 11:16:02 +0530

    api added

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,72 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import axios from 'axios';
 
 function App() {
-let [location,setLocation]=useState('');
-let [submitted,setSubmitted]=useState(false);
-const addLocation=()=>{
-  setSubmitted(true);
-}
+  let [location, setLocation] = useState('');
+  let [submitted, setSubmitted] = useState(false);
+  let [weatherData, setWeatherData] = useState(null);
 
+  const addLocation = () => {
+    setSubmitted(true);
+    fetchWeatherData(); 
+  };
+
+  const fetchWeatherData = async () => {
+    try {
+    
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${
+          import.meta.env.VITE_API_GENERATIVE_LANGUAGE_CLIENT}&units=metric`
+      );
+      const Data = {
+        description: response.data.weather[0].description,
+        temperature: response.data.main.temp,
+        humidity: response.data.main.humidity,
+        cloudiness: response.data.clouds.all
+      };
+
+      setWeatherData(Data);
+    } catch (error) {
+      console.error('Error fetching weather data:', error);
+    }
+  };
 
   return (
     <>
-    <div className="container">
-      <div className="Title">Weather App</div>
+      <div className="container">
+        <div className="Title">Weather App</div>
 
-      {submitted ? (
-        <div className="weather">Your location is {location}</div>
-      ) : (
-        <>
-          <div className="location">
-            <p className="enter">Enter location</p>
-            <input
-              type="text"
-              value={location}
-              onChange={(e) => setLocation(e.target.value)}
-            />
+        {submitted ? (
+          <div className="weather">
+            <p>Your location is {location}</p>
+            {weatherData ? (
+              <>
+                <p>Description: {weatherData.description}</p>
+                <p>Temperature: {weatherData.temperature} °C</p>
+                <p>Humidity: {weatherData.humidity} %</p>
+                <p>Cloudiness: {weatherData.cloudiness} %</p>
+              </>
+            ) : (
+              <p>Loading weather data...</p>
+            )}
           </div>
-          <button onClick={addLocation}>Submit</button>
-        </>
-      )}
-    </div>
-  </>
-  )
+        ) : (
+          <>
+            <div className="location">
+              <p className="enter">Enter location</p>
+              <input
+                type="text"
+                value={location}
+                onChange={(e) => setLocation(e.target.value)}
+              />
+            </div>
+            <button onClick={addLocation}>Submit</button>
+          </>
+        )}
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
